563ac3cd0d14f69bef2d404e714281d0d5136aa8
kapsilon <kapsilon47@gmail.com>
2020-12-09 01:09:13 +0300

    Add LCD Screen Project
    Screen contantacts are limp(((

---
+++ lcd-screen/lcd-screen.ino
@@ -0,0 +1,28 @@
+// Include Library for LCD Screen Using
+#include <LiquidCrystal.h>
+
+// Digital Out Numbers to LCD Screen
+const int RS_PIN = 6;
+const int EN_PIN = 7;
+const int D4_PIN = 8;
+const int D5_PIN = 9;
+const int D6_PIN = 10;
+const int D7_PIN = 11;
+LiquidCrystal lcd(RS_PIN, EN_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
+
+void setup() {
+	// Set LCD Screen's number of Columns and Rows:
+	lcd.begin(16, 2);
+}
+
+void loop() {
+	// Print to First Line
+	lcd.print("Hello!");
+	delay(5000);
+	// Print to Second Line
+	lcd.setCursor(2, 1);
+	lcd.print("You're Super!!!");
+	delay(5000);
+
+	lcd.clear();
+}
