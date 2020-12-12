e5243b9517b7d006710cd086aa9011398d2b10ce
kapsilon <kapsilon47@gmail.com>
2020-12-12 18:43:41 +0300

    Add 7-Segment 4-Digit Display Project

---
+++ segment-display-4/segment-display-4.ino
@@ -0,0 +1,82 @@
+// Digital Out Number to 4-Digit 7-Segment Display (Low Pin Line)
+const int DISPLAY_PIN_01 = 3;
+const int DISPLAY_PIN_02 = 4;
+const int DISPLAY_PIN_03 = 5;
+const int DISPLAY_PIN_04 = 6;
+const int DISPLAY_PIN_05 = 7;
+const int DISPLAY_PIN_06 = 14;
+// Digital Out Number to 4-Digit 7-Segment Display (High Pin Line)
+const int DISPLAY_PIN_07 = 17;
+const int DISPLAY_PIN_08 = 8;
+const int DISPLAY_PIN_09 = 9;
+const int DISPLAY_PIN_10 = 16;
+const int DISPLAY_PIN_11 = 15;
+const int DISPLAY_PIN_12 = 10;
+// Set Segments&Digits by Pins (Just Table)
+const int A = DISPLAY_PIN_08;
+const int B = DISPLAY_PIN_12;
+const int C = DISPLAY_PIN_04;
+const int D = DISPLAY_PIN_02;
+const int E = DISPLAY_PIN_01;
+const int F = DISPLAY_PIN_09;
+const int G = DISPLAY_PIN_05;
+const int DP = DISPLAY_PIN_03;
+const int D1 = DISPLAY_PIN_06;
+const int D2 = DISPLAY_PIN_11;
+const int D3 = DISPLAY_PIN_10;
+const int D4 = DISPLAY_PIN_07;
+
+// Set Variables for loop
+int number;
+int counter = 0;
+
+// Include Library for 4-Digit 7-Segment Display Using
+#include "SevSeg.h"
+SevSeg sevseg;
+
+void setup() {
+	// Set Pins to Output Mode
+	pinMode(A, OUTPUT);
+	pinMode(B, OUTPUT);
+	pinMode(C, OUTPUT);
+	pinMode(D, OUTPUT);
+	pinMode(E, OUTPUT);
+	pinMode(F, OUTPUT);
+	pinMode(G, OUTPUT);
+	pinMode(DP, OUTPUT);
+	pinMode(D1, OUTPUT);
+	pinMode(D2, OUTPUT);
+	pinMode(D3, OUTPUT);
+	pinMode(D4, OUTPUT);
+
+	// SevenSegment Library Settings
+	byte numDigits = 4;
+	byte digitPins[] = {D4, D3, D2, D1};
+	byte segmentPins[] = {A, B, C, D, E, F, G, DP};
+	bool resistorsOnSegments = true;
+	bool updateWithDelaysIn = true;
+	byte hardwareConfig = COMMON_CATHODE;
+	// SevenSegment Library Settings Turn On
+	sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins,
+				 resistorsOnSegments);
+	sevseg.setBrightness(90);
+}
+
+void loop() {
+	// Get 1111,2222...9999 Every Second
+	number = (div(millis(), 1000)).quot * 1111;
+	// Zeroing by Cycle
+	number = number - 9999 * counter;
+	// Set 0000 for 0
+	if (number == 0) {
+		number = 0000;
+	}
+	// Set zeroing for cycle
+	if (number == 9999) {
+		counter += 1;
+	}
+
+	// Show Digits
+	sevseg.setNumber(number, 0);
+	sevseg.refreshDisplay();
+}
