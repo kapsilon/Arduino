ca4b77d1a1c660cdbd7e12e46392aa6b9df78892
kapsilon <kapsilon47@gmail.com>
2020-12-16 00:23:19 +0300

    Add Theremin Project

---
+++ theremin/theremin.ino
@@ -0,0 +1,46 @@
+// Digital Out Number to Buzzer
+const int SPEAKER_PIN = 11;
+
+// Digital Out Number to Distance Meter
+const int DISTANCE_5V = 41;
+const int DISTANCE_TRIG = 39;
+const int DISTANCE_ECHO = 37;
+const int DISTANCE_GND = 35;
+
+//// Include Library for Distance Meter HC-SR04
+#include <HCSR04.h>
+// Set Distance Meter On
+UltraSonicDistanceSensor distanceSensor(DISTANCE_TRIG, DISTANCE_ECHO);
+
+void setup() {
+	// Set Buzzer Pin to Output Mode
+	pinMode(SPEAKER_PIN, OUTPUT);
+
+	// Set Distance Meter Pins to Output Mode
+	pinMode(DISTANCE_5V, OUTPUT);
+	pinMode(DISTANCE_GND, OUTPUT);
+	pinMode(DISTANCE_TRIG, OUTPUT);
+	pinMode(DISTANCE_ECHO, INPUT);
+
+	// DEBUG on Board
+	Serial.begin(9600);
+}
+
+void loop() {
+	// Set Power Supply for Distance Meter
+	digitalWrite(DISTANCE_5V, HIGH);
+	digitalWrite(DISTANCE_GND, LOW);
+
+	// Measure Distance in cantimeters
+	double distance = distanceSensor.measureDistanceCm();
+
+	// Distance out to Serial
+	Serial.print(distance);
+	Serial.println(" cm");
+
+	// Convert cm to note
+	double pitch = distance * 10;
+	tone(SPEAKER_PIN, pitch, 500);
+
+	delay(1000);
+}
